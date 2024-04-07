547911957e543e89971685dc8c77136351310766
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-07 17:31:46 -0300

    init

---
+++ src/routers/Router.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { HomePage } from "../pages/Home"
+
+
+export const Routers = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>        
+    )
+}
