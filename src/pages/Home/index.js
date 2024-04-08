26d7bbe68f1f4758925b344facf5c6dfabbafbaa
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-08 20:32:09 -0300

    feat: Stylized home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,68 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../css/style.css"
+
 export const HomePage = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedArea, setSelectedArea] = useState('');
+    const [showAreaSelect, setShowAreaSelect] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setShowAreaSelect(true);
+            setSuccess(true);
+        }
+    };
+
+    const handleAreaChange = (event) => {
+        setSelectedArea(event.target.value);
+    };
+
+    const handleRemoveFile = () => {
+        setSelectedFile(null);
+        setShowAreaSelect(false);
+        setSuccess(false);
+    };
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="container-fluid">
+            <div className='main'>
+                <h1 className={`fw-bold ${success ? 'text-success' : ''}`}>
+                    {selectedFile ? "Currículo selecionado com sucesso" : "Coloque seu currículo aqui"}
+                </h1>
+                {selectedFile ? (
+                    <div>
+                        <p>Arquivo selecionado: {selectedFile.name}</p>
+                        <button className="btn btn-danger" onClick={handleRemoveFile}>Remover</button>
+                    </div>
+                ) : (
+                    <input
+                        type="file"
+                        placeholder={"Currículo aqui"}
+                        accept=".pdf"
+                        className="input form-control"
+                        onChange={handleFileChange}
+                    />
+                )}
+                {showAreaSelect && (
+                    <div>
+                        <label htmlFor="areaSelect">Selecione sua área de interesse:</label>
+                        <select id="areaSelect" className="form-select" onChange={handleAreaChange} value={selectedArea}>
+                            <option value="">Selecione...</option>
+                            <option value="TI">Tecnologia da Informação</option>
+                            <option value="Engenharia">Engenharia</option>
+                            <option value="Saúde">Saúde</option>
+                        </select>
+                        <br/>
+                        <button className="btn btn-primary">Enviar</button>
+                    </div>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default HomePage;
